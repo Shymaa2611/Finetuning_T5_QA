3ad7cf7d600c516e6232530f573a150e40b60a46
Your Name <you@example.com>
2024-04-23 01:53:05 +0200

    avoid crash

--- data.py
+++ data.py
@@ -3,12 +3,28 @@ import json
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from preprocess import preprocess_data
+def load_json_line_by_line(json_file_path):
+    with open(json_file_path, mode="r") as file:
+        decoder = json.JSONDecoder()
+        buffer = ""
+        for line in file:
+            buffer += line.strip()
+            while buffer:
+                try:
+                    result, index = decoder.raw_decode(buffer)
+                    if isinstance(result, list):
+                        for item in result:
+                            yield item
+                    buffer = buffer[index:]
+                except json.JSONDecodeError:
+                    break
+
 def create_dataset(json_file_path):
     preprocess_data()
+    data=[]
     with open(json_file_path, mode="r") as file:
-        data = json.load(file)
-        print(data)
-
+        for json_obj in load_json_line_by_line(json_file_path):
+               data.append(json_obj)
         csv_file_path = "data.csv"
         field_names = ["question", "answer", "context"]
 
