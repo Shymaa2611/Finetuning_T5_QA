fd8c5a2cbe2409509c745da8c9439bc3512175c7
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-05-04 15:46:24 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,21 +1,21 @@
 import json
-import nltk
 import re
-def preprocess_data():
-   with open('questions_answers.json', 'r') as f:
-     data = json.load(f)
-     processed_data = []
-     for item in data:
-       question = item['question']
-       answer = item['answer']
-       context = item['context']
-       context = re.sub(r'http\S+', '', context)
-       context = re.sub(r'\d+', '', context)
-       context = re.sub(r'[^\w\s]', '', context)
-       
-       processed_data.append({
-        'question': question,
-        'answer': answer,
-        'context': context
-    })
-     return processed_data
+
+def preprocess_data(data):
+    with open('questions_answers.json', 'r') as f:
+        data = json.load(f)
+        processed_data = []
+        for item in data:
+            question = item['question']
+            answer = item['answer']
+            context = item['context']
+            context = re.sub(r'http\S+', '', context)  
+            context = re.sub(r'\d+', '', context)     
+            context = re.sub(r'[^\w\s]', '', context) 
+            context = context.replace('\n', '')        
+            processed_data.append({
+                'question': question,
+                'answer': answer,
+                'context': context
+            })
+        return processed_data
