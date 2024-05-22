86822dcb52e3f5df6037877626466cb72689e214
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-05-22 18:46:45 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,20 +2,18 @@ import json
 import re
 
 def preprocess_data(data):
-    with open('questions_answers.json', 'r') as f:
-        data = json.load(f)
-        processed_data = []
-        for item in data:
-            question = item['question']
-            answer = item['answer']
-            context = item['context']
-            context = re.sub(r'http\S+', '', context)  
-            context = re.sub(r'\d+', '', context)     
-            context = re.sub(r'[^\w\s]', '', context) 
-            context = context.replace('\n', '')        
-            processed_data.append({
-                'question': question,
-                'answer': answer,
-                'context': context
-            })
-        return processed_data
+    processed_data = []
+    for item in data:
+        question = item['question']
+        answer = item['answer']
+        context = item['context']
+        context = re.sub(r'http\S+', '', context)  
+        context = re.sub(r'\d+', '', context)     
+        context = re.sub(r'[^\w\s]', '', context) 
+        context = context.replace('\n', '')        
+        processed_data.append({
+            'question': question,
+            'answer': answer,
+            'context': context
+        })
+    return processed_data
