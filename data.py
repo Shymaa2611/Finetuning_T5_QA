ae281f0b289c39b3e236306458e238e8ad2aef6c
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-05-22 18:45:48 +0300

    Update data.py

--- data.py
+++ data.py
@@ -6,15 +6,18 @@ from preprocess import preprocess_data
 
 def create_dataset(json_file_path):
     with open(json_file_path, 'r') as json_file:
-      data = json.load(json_file)
-      data=preprocess_data(data)
-      csv_file = 'data.csv'
-      fieldnames = data[0].keys()
+        data = json.load(json_file)
+    data = preprocess_data(data)
+    if not data:
+        print("No data after preprocessing.")
+        return
+    csv_file = 'data.csv'
+    fieldnames = data[0].keys()
     with open(csv_file, mode='w', newline='') as file:
-       writer = csv.DictWriter(file, fieldnames=fieldnames)
-       writer.writeheader()
-       for row in data:
-         writer.writerow(row)
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
     print("Data written to CSV file:", csv_file)
 
@@ -25,6 +28,8 @@ def split_data():
     train_df.to_csv("train_data.csv", index=False)
     val_df.to_csv("val_data.csv", index=False)
     print("CSV file split into training and validation sets successfully.")
+
+    
 if __name__ == "__main__":
     json_file_path = "questions_answers.json"
     create_dataset(json_file_path)
